658082f70ec2b3cc14fc6b31b7775d8ac8375900
Andrey <andrey.fafonov2003@mail.ru>
2024-10-02 11:53:49 +0300

    dodelay

--- utility(1).js
+++ utility(1).js
@@ -9,6 +9,7 @@ import { Weapon } from './tribes.js';
 import {
   Apache, Redneci, Weapon, Tools,
 } from './tribes.js';
+import { escape } from 'querystring';
 
 // ф-ция, создающая новый объект класса и сохраняющая его в p.json
 const createObject = () => {
@@ -31,13 +32,46 @@ const createObject = () => {
   setPerson(obj);
 };
 
-const getPath = (fPath) => path.resolve() + fPath;
+// ф-ция, добавляющая конкретный item к конкретному person
+const addItem = () => {
+  const listOfObject = getObject();
 
-const setPerson = (person) => {
-  const path = getPath('/people.json');
-  const peopleList = JSON.parse(fs.readFileSync(path, 'utf-8'));
-  peopleList.alive.push(person);
-  fs.writeFileSync(path, JSON.stringify(peopleList, null, 2), 'utf-8');
+  const listOfNames = listOfObject.alive.map(({name}) => name);
+  const indexOfNames = readlineSync.keyInSelect(listOfNames, 'кому добавить?');
+
+  const listOfitems = listOfObject.item.map(({name}) => name);
+  const indexOfItem = readlineSync.keyInSelect(listOfitems, 'что добавить?');
+
+  const person = backToClass(listOfObject.alive.at(indexOfNames).name);
+  const item = backToClass(listOfObject.item.at(indexOfItem).name);
+  if (person.className == 'Apache' && item.className === 'Weapon') {
+    console.log('невозможно добавить оружие к классу Апаче');
+    return false;
+  }
+  person.addTool(item);
+  updateObject(person);
+  return true;
+}
+
+
+const getPath = () => path.resolve() + fPath;
+
+const getObject = () => JSON.parse(fs.readFileSync(getPath(), 'utf-8'));
+
+const updateJSON = (dataToUpdate) => fs.writeFileSync(getPath(), JSON.stringify(dataToUpdate, null, 2), 'utf-8');
+
+const setObject = (object) => {
+  const listOfObject = getObject();
+  if (['Apache', 'Redneci'].includes(object.className)) {
+    listOfObject.alive.push(object);
+  } else {
+    listOfObject.item.push(object);
+  }
+  updateJSON(listOfObject);
+  // const path = getPath('/data/people.json');
+  // const peopleList = JSON.parse(fs.readFileSync(path, 'utf-8'));
+  // listOfObject.alive.push(object);
+  // fs.writeFileSync(path, JSON.stringify(peopleList, null, 2), 'utf-8');
   // сперва узнаём путь, потом пушим в живых person
 };
 
@@ -66,31 +100,36 @@ const updatePerson = (person) => {
 };
 
 // возвращение объектов json к типу объектов класса
-const backToClass = (name) => {
+const backToClass = (nameToFined) => {
   // читаем json
   const fPath = getPath('/people.json');
   const listOFPerson = JSON.parse(fs.readFileSync(fPath, 'utf-8'));
   // ищем нужный объект
-  const filtered = listOFPerson.alive.filter(({ nameIter }) => name === nameIter).at(0);
+  let filtered = listOFPerson.alive.filter(({ nameIter }) => name === nameToFined).at(0);
+  // console.log(`first search ${filtered}`);
+  if (filtered.length === 0) {
+    filtered = listOFPerson.alive.filter(({ nameIter }) => name === nameToFined).at(0);
+  };
+  const jsonObject = filtered.at(0);
   // [{}] -> {}
   // преобразовываем в объект класса
   let classObject;
-  switch (filtered.className) {
+  switch (filtered.at(0).className) {
     case 'Apache':
-      classObject = new Apache(name);
+      classObject = new Apache(nameToFined);
       break;
     case 'Redneci':
-      classObject = new Redneci(name);
+      classObject = new Redneci(nameToFined);
       break;
     case 'Weapon':
-      classObject = new Weapon(name);
+      classObject = new Weapon(nameToFined);
       break;
     default:
-      classObject = new Tools(name);
+      classObject = new Tools(nameToFined);
       break;
   }
   // указываем конкретные значения ключей
-  const entries = Object.entries(filtered);
+  const entries = Object.entries(jsonObject);
   // [[key, v] [key, v2]...]
   //  for ([key, value] of entries) {
   //     if(_.isObject(value)) {
@@ -114,4 +153,4 @@ setPerson({
   farmingSkill: 73,
 });
 
-export {createObject, deleteDeadPerson, updatePerson};
+export {createObject, deleteDeadPerson, updatePerson, setObject, addItem};
